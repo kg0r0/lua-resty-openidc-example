188cbaf689614a17a2b818d6d85c80ab654d030a
kg0r0 <kg0r0@yahoo.co.jp>
2019-11-15 00:01:27 +0900

    add lua-resty-openidc settings

--- nginx/sample.lua
+++ nginx/sample.lua
@@ -2,6 +2,21 @@ local redis = require "resty.redis"
 local red = redis:new()
 red:set_timeout(3000)
 
+local opts = {
+  ssl_verify = "no",
+  redirect_uri = "https://MY_HOST_NAME/redirect_uri",
+  discovery = "https://accounts.google.com/.well-known/openid-configuration",
+  client_secret = "<client_secret>"
+}
+
+local res, err = require("resty.openidc").authenticate(opts)
+if err then
+  ngx.status = 500
+  ngx.say(err)
+  ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+end
+ngx.req.set_header("X-USER", res.id_token.sub)
+
 local ok, err = red:connect("redis",6379)
 if not ok then
   ngx.say("connection error: ",err)
