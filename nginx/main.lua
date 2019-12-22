68cc12e7fad0d0aceba6074d13cc24438969aff1
kg0r0 <kg0r0@yahoo.co.jp>
2019-12-22 17:43:20 +0900

    use lua-resty-session (#9)

--- nginx/main.lua
+++ nginx/main.lua
@@ -1,29 +1,3 @@
-local redis = require "resty.redis"
-local red = redis:new()
-
-local ok, err = red:connect("redis", 6379)
-if not ok then
-    ngx.say("connection error: ", err)
-    return
-end
-
-local cookie = ngx.var.cookie_sid
-local res, err = red:get(cookie)
-if res then
-    ngx.var.pass = "http://webapp:3000/"
-    return
-end
-
-local is_xhr = false
-if ngx.req.get_headers()["X-Requested-With"] == "XMLHttpRequest" then
-    is_xhr = true 
-end
-
-if is_xhr then
-    ngx.exit(ngx.HTTP_UNAUTHORIZED)
-    return
-end
-
 local opts = {
     ssl_verify = "no",
     redirect_uri = "http://localhost/cb",
@@ -39,15 +13,4 @@ if err then
 end
 ngx.req.set_header("X-USER", res.id_token.sub)
 
-local session_val = "dummy"
-ok, err = red:setex(session_val, 100, res.id_token.sub)
-if not ok then
-    ngx.say("failed to set: ", err)
-    return
-end
-ngx.header['Set-Cookie'] = "sid=" .. session_val .. "; path=/"
-if is_xhr then
-    ngx.exit(ngx.HTTP_OK)
-    return
-end
-ngx.redirect(ngx.var.request_uri)
+ngx.var.pass="http://webapp:3000/"
